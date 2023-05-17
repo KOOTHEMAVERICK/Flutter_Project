86603a12da8cc58daabdffb541ae3e19527e0225
KOOTHEMAVERICK <121041290+KOOTHEMAVERICK@users.noreply.github.com>
2023-05-17 20:48:20 +0900

    Add files via upload

---
+++ common/view/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intermediate/common/const/colors.dart';
+import 'package:intermediate/common/const/data.dart';
+import 'package:intermediate/common/layout/default_layout.dart';
+import 'package:intermediate/common/secure_storage/secure_storage.dart';
+import 'package:intermediate/common/view/root_tab.dart';
+import 'package:intermediate/user/view/login_screen.dart';
+
+class SplashScreen extends ConsumerWidget {
+  static String get routeName => 'splash';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    return DefaultLayout(
+      backgroundColor: PRIMARY_COLOR,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'asset/img/logo.png',
+              width: MediaQuery.of(context).size.width / 2,
+            ),
+            const SizedBox(height: 16.0),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
