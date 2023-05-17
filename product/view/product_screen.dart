49eafaadcdc46ed9e0f7c2139b3d9d0bda4a0633
KOOTHEMAVERICK <121041290+KOOTHEMAVERICK@users.noreply.github.com>
2023-05-17 20:48:47 +0900

    Add files via upload

---
+++ product/view/product_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intermediate/common/component/pagination_list_view.dart';
+import 'package:intermediate/product/model/product_model.dart';
+import 'package:intermediate/product/provider/product_provider.dart';
+import 'package:intermediate/restaurant/view/restaurant_detail_screen.dart';
+import '../component/product_card.dart';
+
+class ProductScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PaginationListView<ProductModel>(
+      provider: productProvider,
+      itemBuilder: <ProductModel>(_, index, model) {
+        return GestureDetector(
+          onTap: () {
+            context.goNamed(RestaurantDetailScreen.routeName, params: {
+              'rid': model.restaurant.id,
+            });
+          },
+          child: ProductCard.fromProductModel(
+            model: model,
+          ),
+        );
+      },
+    );
+  }
+}
